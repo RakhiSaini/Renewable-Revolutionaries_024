c9912e0ac9851ce1130332dec5abdc2fac7c07e9
rambedade <rambedade2308@gmail.com>
2024-10-06 20:51:09 +0530

    added some styles

--- vite-project/src/components/firebase.jsx
+++ vite-project/src/components/firebase.jsx
@@ -5,13 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbGuxaOJXK4tHnn2tUJjetP76XRqNzTcc",
-  authDomain: "hack-hurdles-036.firebaseapp.com",
-  databaseURL: "https://hack-hurdles-036-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hack-hurdles-036",
-  storageBucket: "hack-hurdles-036.appspot.com",
-  messagingSenderId: "832224531387",
-  appId: "1:832224531387:web:88361bcccc83c6de4bea9e"
+  apiKey: "AIzaSyBCCiGU2Z6YRNJwQ8m1behr58MPRc-QiPU",
+  authDomain: "present-flow.firebaseapp.com",
+  projectId: "present-flow",
+  storageBucket: "present-flow.appspot.com",
+  messagingSenderId: "597815501440",
+  appId: "1:597815501440:web:9d88edc6b876633ad1fc65"
 };
 
 // Initialize Firebase only if no apps are already initialized
